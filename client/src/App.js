7ae73522047727ae4a619ea6032c4e73743da815
Dmitry <spaser15@live.ru>
2018-02-04 22:47:30 +0200

    Repair

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,21 @@
-import React, {Component} from 'react';
-import Reboot from 'material-ui/Reboot';
-import Navbar from './AppBar/AppBar';
+import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-	render() {
-		return (
-			<div className="App">
-				<Reboot />
-				<Navbar />
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
 }
 
 export default App;
